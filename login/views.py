750fb76c341307e60c9b4cc16fe3dbfbd3abdc04
Silvia11 <2019202321@ruc.edu.cn>
2021-12-06 21:02:34 +0800

    登录情况说明

--- login/views.py
+++ login/views.py
@@ -1,22 +1,21 @@
-
 # Create your views here.
-from django.shortcuts import render, redirect
-#from django.http import HttpResponseRedirect
+
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth import authenticate, login, logout
-#from django.contrib.auth.models import AnonymousUser
-from rest_framework.authtoken.views import APIView,AuthTokenSerializer
-from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import APIView, AuthTokenSerializer
+#from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from django.contrib.auth.hashers import check_password
 from django.db import connection
 import json
 
+
 # Create your views here.
 # 用户注册
-#在内置的User中username,定义为email
+# 在内置的User中username,定义为email
 class Register(APIView):
-    def post(self,request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         if request.method == 'POST':
 
             print("receive POST request at /register")
@@ -33,29 +32,29 @@ class Register(APIView):
                 resp = {
                     'id': -1,
                     'msg': 'Username already exists',
-                    'payload': [],
                 }
             else:
                 user = User.objects.create_user(username=email, password=password)
-                with connection.cursor() as cur:
-                    cur.execute()
-
+                operation_insert = 'insert into mall1.users(user_id,username,mobile,province,city,address) values(%s,%s,%s,%s,%s,%s)'
+                cursor = connection.cursor()
+                cursor.execute(operation_insert, [email, username, mobile, province, city, address])
                 resp = {
                     'id': 0,
                     'msg': 'Success',
                 }
+                cursor.close()
 
         return Response(resp)
 
+
 # 用户登录
 class Login(APIView):
-    def post(self,request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         if request.method == 'POST':
             print("receive POST request at /login")
             data = json.loads(request.body)
             print(data)
-
-            username = data.get('username')
+            username = data.get('username')#actually email
             password = data.get('password')
             print(username)
             print(password)
@@ -63,72 +62,59 @@ class Login(APIView):
             #   username = auth.authenticate(username = username, password = password)
 
             n = authenticate(username=username, password=password)
-            if n:
+            print(n)
+            if n is not None:
                 # 登陆成功即可获取当前登录用户，返回主页
                 auth.login(request, user=n)
-                #return
+                operation_select = 'select'
+                # return
                 resp = {
                     'id': 0,
                     'msg': 'Success',
-                    'payload':[]
+                    'payload': {
+                        "username": "A",
+                        "mobile": "10.46.233.207",
+                        "province": "D",
+                        "city": "1",
+                        "address": "1"
+                    }
                 }
                 return Response(resp)
-        # 失败重定向到登录页
-            #test----------------------------------------------
+            # 失败重定向到登录页
+            # test----------------------------------------------
             else:
-                resp = {
-                    'id': -1,
-                    'msg': 'username doesnot exist',
-                    'payload':
-                        {
-                            "username": "A",
-                            "mobile": "10.46.233.207",
-                            "province": "D",
-                            "city": "1",
-                            "address": "1"
+                user = User.objects.filter(username=username)
+
+                print(user)
+
+                if len(user) != 0:
+                    pwd = user.password
+                    if check_password(password, pwd) is False:
+                        resp = {
+                            "id": -2,
+                            "msg": "password incorrect"
                         }
-                }
+
+                else:
+                    resp = {
+                        "id": -1,
+                        "msg": "username doesn't exist"
+                    }
+
                 return Response(resp)
-            #test----------------------------------------------
-        #return
-        return redirect('/api/test/')
 
 
 class Test(APIView):
-    def get(self,request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
         return Response("hello world")
-# # 主页
-# def index(request, ):
-#     username = request.user
-#     return render(request, 'login/index.html', locals())
-#
-#
-# 登录
 
-
-## 注册
-# def register(request):
-#     if request.method == 'POST' and request.POST:
-#         data = request.POST
-#         username = data.get("username")
-#         password = data.get("password")
-#         # 校验注册，名字不可重复
-#         u = User.objects.filter(username=username).first()
-#         if u:
-#             info = '该用户名已被注册'
-#             return render(request, 'login/ERROR.html', {'info': info})
-#         else:
-#             # 注册成功，创建用户
-#             User.objects.create_user(
-#                 username=username,
-#                 password=password
-#             )
-#             # 重定向到登录页面
-#             return HttpResponseRedirect('/tologin/')
-#     # 注册失败，重新注册
-#     return render(request, 'login/register.html')
-#
-#
-# def lagout(request):
-#     logout(request)
-#     return redirect('/')
+# sql_insert = 'insert into mall.users(user_id,name,password,email,mobile,province,city) values(%s,%s,%s,%s,%s,%s,%s)'
+# cursor = connection.cursor()
+# cursor.execute(sql_insert, ['000000000000002', 'sy', '12345', '200001@ruc.edu.cn', '13767595949', 'jiangxi', 'gaoan'])
+# sql_select = 'select * from mall.users'
+# cursor.execute(sql_select)
+# rows = cursor.fetchall()
+# for row in rows:
+#   print(row)
+# cursor.execute()
+# cursor.close()
