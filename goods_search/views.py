e4a6cb00a7f5d29b47f060953fbf961c61b43cfe
Voldemore <ruczzq@ruc.edu.cn>
2021-12-08 02:46:02 +0800

    修改了goods_search界面。 一个MySQL的表需要改一下。 goods_management我想不好该怎么改。 或许可以先填一下数据库？爬的可以用了，不然检测困难

--- goods_search/views.py
+++ goods_search/views.py
@@ -15,7 +15,26 @@ from django.db import connection
 import json
 
 
+class GoodsSearch(APIView):
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            print("receive POST request at /goods_search")
+            data = json.loads(request.body)
+            goods_id = data.get('goods_id')
+            mer_id = data.get('mer_id')
+
+        n = authenticate(goods_id=goods_id, mer_id=mer_id)
+        print(n)
+        if n is not None:
+            # 登陆成功即可获取当前登录用户，返回主页
+            auth.login(request, user=n)
+            operation_select = 'select goods_name,mer_name,goods_image,price,sales,description,comments,stock from mergoods where goods_id = %s and mer_id = %s'
+
+            #写到这儿了，表mergoods需要加上这些元素，这个表专门服务goodssearch最好
+
+            #下面都不是
+
 
 class Test(APIView):
     def get(self, request, *args, **kwargs):
-        return Response("小笨蛋")
+        return Response("困！")
