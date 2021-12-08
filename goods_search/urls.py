8573ff09efd1a4f093d121d2794bc8455e1e4b8c
Voldemore <ruczzq@ruc.edu.cn>
2021-12-09 02:44:45 +0800

    一个中间步骤

--- goods_search/urls.py
+++ goods_search/urls.py
@@ -1,8 +1,7 @@
-#python manage.py runserver 0.0.0.0:8000
-from django.urls import path
-from goods_search import views
+from django.urls import path, include, re_path
+from order_search import views
 
 urlpatterns = [
-    #path(r'api/orders/$',views.order_inquiry.as_view())
+    path(r'goods_search/',views.GoodsSearch.as_view())
 
 ]
--- order_search/urls.py
+++ order_search/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from order_search import views
 
 urlpatterns = [
     path(r'inquiry/',views.order_inquiry.as_view())
 
 ]
-
