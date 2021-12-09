9a0887cd3619bfb990a720e2a78bc66faea41151
Silvia11 <2019202321@ruc.edu.cn>
2021-12-10 00:06:42 +0800

    完成商家注册与登录基本功能，但是查询可能存在问题是否需要创建视图的问题

--- login_merchant/urls.py
+++ login_merchant/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from login_merchant import views
 
 urlpatterns = [
-    path(r'register/', views.Register.as_view())
+    path(r'register/', views.Register.as_view()),
+    path(r'login/',views.Login.as_view())
 
 ]
--- login_merchant/views.py
+++ login_merchant/views.py
@@ -7,7 +7,6 @@ import datetime
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib.auth import authenticate, login, logout
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.authtoken.views import APIView, AuthTokenSerializer
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
@@ -25,8 +24,7 @@ class Register(APIView):
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
 
-            print("receive POST request at /register")
-
+            print("receive POST request at /merchant/register")
             data = json.loads(request.body)
             email = data.get('email')
             password = data.get('password')
@@ -42,8 +40,8 @@ class Register(APIView):
                     'msg': 'Username already exists',
                 }
             else:
-                user = User.objects.create_user(username=email, password=password, is_staff=0)
-                operation_insert = 'insert into mall1.users(user_id,username,mobile,province,city,address) values(%s,%s,%s,%s,%s,%s)'
+                user = User.objects.create_user(username=email, password=password, is_staff=1)
+                operation_insert = 'insert into mall1.merchant(mer_id,name,mobile,province,city,address) values(%s,%s,%s,%s,%s,%s)'
                 cursor = connection.cursor()
                 cursor.execute(operation_insert, [email, username, mobile, province, city, address])
                 resp = {
@@ -59,7 +57,7 @@ class Register(APIView):
 class Login(APIView):
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
-            print("receive POST request at /login_customer")
+            print("receive POST request at /merchant/login")
             data = json.loads(request.body)
             print(data)
             email = data.get('username')  # actually email
@@ -74,33 +72,29 @@ class Login(APIView):
             if n is not None:
                 # 登陆成功即可获取当前登录用户，返回主页
                 auth.login(request, user=n)
-                operation_select = 'select username,mobile,province,city,address from mall1.users where user_id = %s'
+                operation_select = 'select name,mobile,province,city,address from mall1.merchant where mer_id = %s'
                 cursor = connection.cursor()
                 cursor.execute(operation_select, [email])
                 result = cursor.fetchone()
 
-                dict_res = {'time': datetime.datetime.now(), 'username': result[0], 'mobile': result[1],
-                            'province': result[2], 'city': result[3],
-                            'address': result[4], }
+                dict_res = {
+                    'time': datetime.datetime.now(),
+                    'username': result[0],
+                    'mobile': result[1],
+                    'province': result[2],
+                    'city': result[3],
+                    'address': result[4], }
                 # return
                 resp = {
                     'id': 0,
                     'msg': 'Success',
                     'payload': dict_res
-                    #     {
-                    #     "username": "A",
-                    #     "mobile": "10.46.233.207",
-                    #     "province": "D",
-                    #     "city": "1",
-                    #     "address": "1"
-                    # }
                 }
                 return Response(resp)
             # 失败重定向到登录页
             # test----------------------------------------------
             else:
                 user = User.objects.filter(username=email)
-
                 print(user)
 
                 if len(user) != 0:
@@ -122,9 +116,7 @@ class Login(APIView):
                 return Response(resp)
 
 
-class Test(APIView):
-    def get(self, request, *args, **kwargs):
-        return Response("hello world")
+
 
 # sql_insert = 'insert into mall.users(user_id,name,password,email,mobile,province,city) values(%s,%s,%s,%s,%s,%s,%s)'
 # cursor = connection.cursor()
