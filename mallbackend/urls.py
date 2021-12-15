5ab98c29c2dd3cf10b530cd2eebb2b3f500331cd
Voldemore <ruczzq@ruc.edu.cn>
2021-12-15 20:52:21 +0800

    开始做query[2]

--- mallbackend/urls.py
+++ mallbackend/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path(r'api/goods_search/', include('goods_search.urls')),
     path(r'api/merchant/', include('login_merchant.urls')),
     path(r'api/cart', include('cart.urls')),
+    path(r'api/query', include('query.urls')),
 ]
--- query/views.py
+++ query/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+import datetime
+
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.views import APIView, AuthTokenSerializer
+from rest_framework.response import Response
+from django.db import connection
+import json
+import pymysql
+from SQL_connection.sqlhelper import SqlHelper
+
+
+class MerQuery(APIView):
+    []
+
+class GoodsQuery(APIView):
+    []
