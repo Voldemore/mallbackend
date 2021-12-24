782c657f7877ece8623f7f74886188f6257594c7
Voldemore <ruczzq@ruc.edu.cn>
2021-12-24 21:27:43 +0800

    商家信息更新；添加shopname

--- login_merchant/views.py
+++ login_merchant/views.py
@@ -19,7 +19,7 @@ from SQL_connection.sqlhelper import SqlHelper
 # Create your views here.
 # 在内置的User中username,定义为email
 
-# 商家注册
+# 商家注册，更新shopname
 class Register(APIView):
 
     def post(self, request, *args, **kwargs):
@@ -29,8 +29,9 @@ class Register(APIView):
             # handle the request
             data = json.loads(request.body)
             email = data.get('email')
+            mername = data.get('mername')
+            shopname = data.get('shopname')
             password = data.get('password')
-            username = data.get('name')
             mobile = data.get('mobile')
             province = data.get('province')
             city = data.get('city')
@@ -45,9 +46,10 @@ class Register(APIView):
             else:
                 user = User.objects.create_user(username=email, password=password, is_staff=1)
                 obj = SqlHelper()
-                operation_insert = 'insert into mall.merchant(mer_id,name,mobile,province,city,county,address) values(%s,%s,%s,%s,%s,%s,%s)'
+                operation_insert = 'insert into mall.merchant(mer_id,name,mobile,province,city,county,address,shopname) ' \
+                                   'values(%s,%s,%s,%s,%s,%s,%s,%s)'
 
-                obj.modify(operation_insert, [email, username, mobile, province, city, county, address, ])
+                obj.modify(operation_insert, [email, mername, mobile, province, city, county, address, shopname, ])
                 resp = {
                     'id': 0,
                     'msg': 'Success',
@@ -57,14 +59,14 @@ class Register(APIView):
         return Response(resp)
 
 
-# 商家登录
+# 商家登录，更新shopname
 class Login(APIView):
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
             print("receive POST request at /merchant/login")
             data = json.loads(request.body)
             print(data)
-            email = data.get('mername')  # actually email
+            email = data.get('username')  # actually email
             password = data.get('password')
             print(email)
             print(password)
@@ -77,7 +79,7 @@ class Login(APIView):
                 user = User.objects.get(username=email)
                 staff_state = user.is_staff
                 if staff_state == 1:
-                    operation_select = "select name,mobile,province,city,county,address from mall.view_merchant_users where mer_id = %s"
+                    operation_select = "select name,shopname,mobile,province,city,county,address from mall.merchant where mer_id = %s"
                     obj = SqlHelper()
                     result = obj.get_one(operation_select, [email, ])
                     obj.close()
@@ -128,20 +130,22 @@ class Login(APIView):
 
 
 # 获取商家信息
+
+# 暂时没有用
 class Merchant_Info(APIView):
     def get(self, request, *args, **kwargs):
         if request.method == 'GET':  # 要求使用GET请求方式
             print("receive GET request at /merchant_info")
             data = request.GET  # 处理请求
-            mer_id = int(data.get('mername'))
+            mer_id = data.get('email')
             print(mer_id)
             user = User.objects.filter(username=mer_id)
             if user is not None:
                 user = User.objects.get(username=mer_id)
                 if user.is_staff == 1:
                     obj = SqlHelper()
-                    sql_select = 'select mer_id,name,mobile,province,city,county,address ' \
-                                 'from mall.view_merchant_users ' \
+                    sql_select = 'select mer_id,shopname,name,mobile,province,city,county,address ' \
+                                 'from mall.merchant ' \
                                  'where mer_id = %s'
                     result = obj.get_one(sql_select, [mer_id, ])
                     result['time'] = datetime.datetime.now()
@@ -172,12 +176,13 @@ class Info_Mod(APIView):
             data = json.loads(request.body)
             email = data.get('email')
             password = data.get('password')
-            name = data.get('name')
+            name = data.get('mername')
             mobile = data.get('mobile')
             province = data.get('province')
             city = data.get('city')
             county = data.get('county')
             address = data.get('address')
+            shopname = data.get('shopname')
 
             # 修改密码
 
@@ -187,8 +192,8 @@ class Info_Mod(APIView):
 
             # 其他部分
             obj = SqlHelper()
-            info_update = 'update mall.merchant set name=%s, mobile=%s, province=%s, city=%s, county=%s, address=%s'
-            obj.modify(info_update, [name, mobile, province, city, county, address, ])
+            info_update = 'update mall.merchant set name=%s, mobile=%s, province=%s, city=%s, county=%s, address=%s, shopname=%s'
+            obj.modify(info_update, [name, mobile, province, city, county, address, shopname, ])
             resp = {
                 'id': 0,
                 'msg': 'Success',
